e64676ec01381f7686aa0bcf695b78dac13e48f2
Heidel <136027069+heidelpants@users.noreply.github.com>
2023-07-24 13:50:54 -0500

    Update index.js
    added comments for assistance in future coding
    may need to be more specific/could be somewhat incorrect

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
+/*Import Firebase information to pull from*/
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
+/*Initialize Firebase "assSettings" from personal Firebase Realtime Database*/
 const appSettings = {
     databaseURL: "https://realtime-database-28ef7-default-rtdb.firebaseio.com/"
 }
 
+/*Initialize (pull) information from Firebase & reference specific Firebase Realtime Database*/
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shoppingListInDB = ref(database, "shoppingList")
 
+/*Initialize initial variables for "Add to Cart" App*/
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const shoppingListEl = document.getElementById("shopping-list")
 
+/*create event listener to give "Add Button" a purpose*/
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     
@@ -21,6 +26,7 @@ addButtonEl.addEventListener("click", function() {
     clearInputFieldEl()
 })
 
+/*onValue ?method? ?function? to have a constant line of communication to specific Firebase Realtime Database to push and pull changes to database info*/
 onValue(shoppingListInDB, function(snapshot) {
     if (snapshot.exists()) {
         let itemsArray = Object.entries(snapshot.val())
@@ -39,14 +45,17 @@ onValue(shoppingListInDB, function(snapshot) {
     }
 })
 
+/*this function clears a/the Shopping List Element*/
 function clearShoppingListEl() {
     shoppingListEl.innerHTML = ""
 }
 
+/*this function clears the Input field Element*/
 function clearInputFieldEl() {
     inputFieldEl.value = ""
 }
 
+/*this APPEND-function adds a new item (array) to the database and creates a reference with its unique identifier to remove/add items to list*/
 function appendItemToShoppingListEl(item) {
     let itemID = item[0]
     let itemValue = item[1]
